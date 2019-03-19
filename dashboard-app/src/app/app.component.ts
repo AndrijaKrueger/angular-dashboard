74f4842f8242530ac90459755f5ce86ab0818ded
Andrija Krüger <andrija.krueger@amdis-services.com>
2019-03-19 10:03:39 +0100

    letzte anpassungen

--- dashboard-app/src/app/app.component.ts
+++ dashboard-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dashboard-app';
+  title = 'Dashboard';
+
+us  todayTemp = new Date();
+  time = '';
+  date2 = '';
+
+  map = [
+    {
+      key: 0,
+      day: 'Sonntag'
+    }, {
+      key: 1,
+      day: 'Montag'
+    }, {
+      key: 2,
+      day: 'Dienstag'
+    }, {
+      key: 3,
+      day: 'Mittwoch'
+    }, {
+      key: 4,
+      day: 'Donnerstag'
+    }, {
+      key: 5,
+      day: 'Freitag'
+    }, {
+      key: 6,
+      day: 'Samstag'
+    },
+  ];
+
+
+  today = setInterval(() => {
+    this.todayTemp = new Date();
+    this.time = this.todayTemp.toLocaleTimeString();
+    this.date2 = this.map[this.todayTemp.getDay()].day + ' ' + this.todayTemp.toLocaleDateString();
+
+  }, 1000);
+
+
 }
