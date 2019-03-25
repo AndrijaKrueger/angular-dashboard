53ab24b6cd8c7561b0ab4e7245a0d7ca37e8fcac
Andrija Krüger <andrija.krueger@amdis-services.com>
2019-03-25 10:34:14 +0100

    blub

--- dashboard-app/src/app/app.component.ts
+++ dashboard-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'Dashboard';
 
-us  todayTemp = new Date();
+  todayTemp = new Date();
   time = '';
   date2 = '';
 
@@ -37,6 +38,58 @@ us  todayTemp = new Date();
     },
   ];
 
+  dummies = [
+    {
+      title: 'MÄRKISCHES WINTERAUSTREIBEN',
+      place: 'Museums Dorf Düppel',
+      days: this.getDays(1553943600),
+      dateFrom: this.getFormatedDated(1553943600)
+    }, {
+      title: 'Die Toten Hosen',
+      place: 'Zoo Palast Kino 1',
+      days: this.getDays(1554322500),
+      dateFrom: this.getFormatedDated(1554322500)
+    }, {
+      title: 'Harz Urlaub 2019',
+      place: '38871 Ilsenburg',
+      days: this.getDays(1555146000),
+      dateFrom: this.getFormatedDated(1555146000)
+    }, {
+      title: 'Winter is coming ... Finale',
+      place: 'Sky',
+      days: this.getDays(1555232400),
+      dateFrom: this.getFormatedDated(1555232400)
+    }, {
+      title: 'Kite Kurs',
+      place: 'Zingst, Campingplatz Prerow',
+      days: this.getDays(1556643600),
+      dateFrom: this.getFormatedDated(1556643600)
+    }, {
+      title: 'Ritterfest',
+      place: 'Museums Dorf Düppel',
+      subTitle: '18.05. und 19.05.2019 10 - 18 Uhr',
+      days: this.getDays(1558180800),
+      dateFrom: this.getFormatedDated(1558180800)
+    }, {
+      title: 'Pfingsten Ostsee Wochenende',
+      place: 'Ostseecamp Seeblick',
+      days: this.getDays(1559980800),
+      dateFrom: this.getFormatedDated(1559980800)
+    }, {
+      title: 'Sommer Urlaub',
+      place: 'Köln - Speyer - Plansee - Venedig',
+      subTitle: '22.6. - 8.7. 2019',
+      days: this.getDays(1561190400),
+      dateFrom: this.getFormatedDated(1561190400)
+    }, {
+      title: 'Das Laternenfest',
+      place: 'Halle - Ein Fest mit Tradition',
+      subTitle: '23. - 25. August 2019',
+      days: this.getDays(1566579600),
+      dateFrom: this.getFormatedDated(1566579600)
+    }
+  ];
+
 
   today = setInterval(() => {
     this.todayTemp = new Date();
@@ -44,6 +97,33 @@ us  todayTemp = new Date();
     this.date2 = this.map[this.todayTemp.getDay()].day + ' ' + this.todayTemp.toLocaleDateString();
 
   }, 1000);
+
+
+  getDays(dateTimeStamp) {
+    const startDate = this.todayTemp;
+    console.log('startDate: ' + startDate);
+
+    const endDate = this.convertTimeSTampToDate(dateTimeStamp);
+    console.log('endDate: ' + endDate);
+
+    const temp = endDate.valueOf() - startDate.valueOf();
+    console.log('temp: ' + temp);
+
+    const result = Math.round((temp) / (1000 * 60 * 60 * 24));
+    console.log('result: ' + result);
+
+    return result;
+  }
+
+  getFormatedDated(dateTimeStamp) {
+    const date = this.convertTimeSTampToDate(dateTimeStamp);
+
+    return this.map[date.getDay()].day + ' ' + date.toLocaleDateString();
+  }
+
+  convertTimeSTampToDate(timeStamp) {
+    return new Date(timeStamp * 1000);
+  }
 
 
 }
